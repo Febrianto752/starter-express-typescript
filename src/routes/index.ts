329950642d1915ad3f8584f5b2fe579609b786bc
Febrianto752 <febrianto.bekasi@gmail.com>
2024-12-16 11:37:06 +0700

    feat: update script package.json and update gitignore content
    update script build and start, ignore .env file, install cross-env and rimraf package, and remove build-on-windows script in package.json

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req: Request, res: Response, next: NextFunction) {
-  res.status(200).json({ messsage: 'hello world' });
+  return res.status(200).json({ messsage: 'hello world' });
 });
 
 export default router;
